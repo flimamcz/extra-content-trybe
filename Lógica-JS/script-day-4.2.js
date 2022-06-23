c270ab7cdcc8d4db6e75ebbc8955169b987bbb72
Filipe Lima <cimofelipe4@gmail.com>
2022-06-23 18:59:52 -0300

    Add challenge day 4.2

---
+++ Lógica-JS/script-day-4.2.js
@@ -0,0 +1,57 @@
+// const marina = 25
+// const silvia = 18
+// const iza = 20
+
+// switch(true) {
+//   case (marina < iza && marina < silvia ):
+//     console.log('marina')
+//     break
+
+//     case (silvia < iza && silvia < marina ):
+//     console.log('silvia')
+//     break
+
+//   default:
+//     console.log('iza')
+// }
+
+// const age = 20
+// const sex = 'M'
+// const weight = 60
+// const height = 172
+
+// let tmb
+
+// if (sex.toLowerCase() === 'f'){
+//   tmb = (height * 6.25) + (weight * 9.99) - (idade * 4.92) - 161
+//   console.log()
+// }
+
+const number = 2;
+
+switch (number) {
+  case 1:
+    console.log("1 - Trybe Lanche Feliz");
+    break;
+
+  case 2:
+    console.log("2 - McTrybe");
+    break;
+
+  case 3:
+    console.log("3 - TrybeWooper");
+    break;
+
+  case 4:
+    console.log("4 - X-Trybe");
+    break;
+
+  case 5:
+    console.log("5 - Triplo Trybe com JS");
+    break
+  
+  default:
+    console.log("Ainda não possuímos esta opção :(");
+    
+}
+
